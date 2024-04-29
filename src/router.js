2cc042b9ce8bc2dc00880cd944acbc4c1558af96
Alejandro Novellino <alnovellino07@gmail.com>
2024-04-28 23:50:40 -0400

    update router

--- src/router.js
+++ src/router.js
@@ -3,12 +3,24 @@ import { createBrowserRouter } from "react-router-dom";
 // Components and pages imports
 import ErrorInfo from "./presentation/common/pages/ErrorInfo";
 import Landing from "./presentation/common/pages/Landing";
+import Home from "./presentation/common/pages/Home";
+import Welcome from "./presentation/common/pages/Welcome";
 
 // router creation
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Landing />,
+		errorElement: <ErrorInfo />,
+	},
+	{
+		path: "/welcome",
+		element: <Welcome />,
+		errorElement: <ErrorInfo />,
+	},
+	{
+		path: "/home",
+		element: <Home />,
 		errorElement: <ErrorInfo />,
 	},
 ]);
