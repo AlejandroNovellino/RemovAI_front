924b36870cf1fc422b5ae7626097beb3c0ca5236
Alejandro Novellino <alnovellino07@gmail.com>
2024-05-06 22:10:21 -0400

    update cards

--- src/presentation/common/components/HomeMovieCard.jsx
+++ src/presentation/common/components/HomeMovieCard.jsx
@@ -1,3 +1,5 @@
+// react imports
+import { useState } from "react";
 // react bootstrap imports
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -12,7 +14,12 @@ function HomeMovieCard({
 	dislikeMovie,
 	handleMovieSelection,
 }) {
+	// state for hovering
+	const [isHovering, setIsHovering] = useState(false);
 	let liked = false;
+
+	const handleMouseEnter = () => setIsHovering(true);
+	const handleMouseLeave = () => setIsHovering(false);
 
 	if (
 		likedMovies &&
@@ -31,32 +38,45 @@ function HomeMovieCard({
 
 	return (
 		<>
-			<Card bg="dark" className="text-center">
+			<Card
+				bg="dark"
+				className="text-center"
+				onMouseEnter={handleMouseEnter}
+				onMouseLeave={handleMouseLeave}>
 				<Card.Img
 					src={`https://image.tmdb.org/t/p/w500${movie.cartel_path}`}
 					alt="Card image"
 				/>
-				<Card.ImgOverlay>
-					<Container>
-						<Row>
-							<Col>
-								<Button
-									variant="dark"
-									onClick={() => handleMovieSelection(movie)}>
-									<i className="bi bi-info-circle tw-text-purple-200"></i>
-								</Button>
-							</Col>
-							<Col>
-								<Button variant="dark" onClick={handleLike}>
-									{liked ? (
-										<i className="bi bi-hand-thumbs-up-fill tw-text-purple-200"></i>
-									) : (
-										<i className="bi bi-hand-thumbs-up"></i>
-									)}
-								</Button>
-							</Col>
-						</Row>
-					</Container>
+				<Card.ImgOverlay
+					className={isHovering || liked ? "tw-bg-neutral-900/[.60]" : ""}>
+					{(isHovering || liked) && (
+						<Container fluid className="h-100 w-100">
+							<Row className="h-50 p-4">
+								<Col>
+									<Button
+										className="h-100 w-100  tw-bg-neutral-900/[.75]"
+										variant="dark"
+										onClick={() => handleMovieSelection(movie)}>
+										<i className="h1 bi bi-info-circle tw-text-purple-200"></i>
+									</Button>
+								</Col>
+							</Row>
+							<Row className="h-50 p-4">
+								<Col>
+									<Button
+										className="h-100 w-100 tw-bg-neutral-900/[.75]"
+										variant="dark"
+										onClick={handleLike}>
+										{liked ? (
+											<i className="h1 bi bi-hand-thumbs-up-fill tw-text-purple-200"></i>
+										) : (
+											<i className="h1 bi bi-hand-thumbs-up"></i>
+										)}
+									</Button>
+								</Col>
+							</Row>
+						</Container>
+					)}
 				</Card.ImgOverlay>
 			</Card>
 		</>
